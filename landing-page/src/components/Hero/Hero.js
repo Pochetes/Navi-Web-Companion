06d40e9ab549d0e68ffbeb17237a74541cfbf8d3
Pochetes <robertomiguel2001@gmail.com>
2021-09-25 01:55:39 -0400

    added header and hero components w/o design

---
+++ landing-page/src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <>
+            <div className="hero-layout">
+                <div className="hero-title">
+                    <h1>The Web Companion We All Needed.</h1>
+                </div>
+                <div className="hero-btn">
+                    <button>Install Now</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero
