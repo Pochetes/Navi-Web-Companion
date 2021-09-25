99413cc369e58dea548cd9fe3e151d40ad1c4723
Pochetes <robertomiguel2001@gmail.com>
2021-09-25 12:16:13 -0400

    improved design of website

--- landing-page/src/components/Hero/Hero.js
+++ landing-page/src/components/Hero/Hero.js
@@ -7,8 +7,19 @@ function Hero() {
                 <div className="hero-title">
                     <h1>The Web Companion We All Needed.</h1>
                 </div>
-                <div className="hero-btn">
-                    <button>Install Now</button>
+                <div className="hero-desc">
+                    <p>
+                         <span className="hero-desc-arrow">--></span> A tool that helps you navigate the web.
+                    </p>
+                    <p>
+                    <span className="hero-desc-arrow">--></span> Spits out a summary of the contents using Artificial Intelligence.
+                    </p>
+                    <p>
+                    <span className="hero-desc-arrow">--></span> Chat-Bot Interface for a great experience.
+                    </p>
+                </div>
+                <div className="hero-btn-container">
+                    <button className="hero-btn">Install Now</button>
                 </div>
             </div>
         </>
