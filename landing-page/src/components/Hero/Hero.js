7c04addb889699c7ba94ee3be4ce18b2be50236e
Pochetes <robertomiguel2001@gmail.com>
2021-09-25 22:41:52 -0400

    added other designs to landing page

--- landing-page/src/components/Hero/Hero.js
+++ landing-page/src/components/Hero/Hero.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import circle_right from '../../icons/circle_right.svg'
+import striped_circle from '../../icons/striped_circle.svg'
+import striped_star from '../../icons/striped_star.svg'
 import { motion } from 'framer-motion';
 
 function Hero() {
     return (
         <>
+            <motion.div 
+            className="striped-shapes"
+            >
+                <img className="striped-circle" src={striped_circle} alt="cool-circle"/>
+            </motion.div>
+
+
+            <motion.div
+            className="striped-shapes"
+            >
+                <img className="striped-star" src={striped_star} alt="cool-star"/>
+            </motion.div>
+
             <div className="hero-layout">
                 <div className="hero-title">
                     <motion.h1 
@@ -19,27 +34,27 @@ function Hero() {
                     animate={{ x: [1000, -100, 0] }}
                     transition={{ duration: 1 }}
                     >
-                        <img src={circle_right}/> A tool that helps you navigate the web.
+                        <img src={circle_right} alt="circle-arrow"/> A tool that helps you navigate the web.
                     </motion.p><br></br>
                     <motion.p
                     animate={{ x: [1000, -100, 0] }}
                     transition={{ duration: 2 }}
                     >
-                        <img src={circle_right}/> Spits out a summary of the contents using Artificial Intelligence.
+                        <img src={circle_right} alt="circle-arrow"/> Spits out a summary of the contents using Artificial Intelligence.
                     </motion.p><br></br>
                     <motion.p 
                     animate={{ x: [1000, -100, 0] }}
                     transition={{ duration: 3 }}
                     >
-                        <img src={circle_right}/> Chat-Bot Interface for a great experience.
+                        <img src={circle_right} alt="circle-arrow"/> Chat-Bot Interface for a great experience.
                     </motion.p><br></br>
                 </div>
                 <div className="hero-btn-container">
                     <motion.button 
                     className="hero-btn"
                     animate={{ y: [600, 0] }}
-                    transition={{ duration: 0.2 }}
                     whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
                     >
                         Install Now
                     </motion.button>
