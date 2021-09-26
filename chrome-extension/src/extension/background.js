c118672e633c2a5012f36f9db2f5fb08ddb81e56
David <drvillase@gmail.com>
2021-09-25 21:46:26 -0400

    Updated

---
+++ chrome-extension/src/extension/background.js
@@ -0,0 +1,10 @@
+const SummaryBot = require("summarybot");
+const summaryBot = new SummaryBot();
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({ id: "summarize", title: "Summarize", contexts: [ "selection" ] });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    chrome.tabs.sendMessage(tab.id, { content: summaryBot.run(info.selectionText, 4, false) });
+});
